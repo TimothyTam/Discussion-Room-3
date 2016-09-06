4e85455d3ae33b40c7fd705d2e0f81b45d237d89
Kou <kkoujm@gmail.com>
2016-09-06 21:44:41 +0800

    Add TNode default constructor

--- source/TNode.h
+++ source/TNode.h
@@ -3,7 +3,6 @@
 #include <vector>
 
 enum class NodeType { Program, Procedure, StmtLst, Assign, Variable, Plus, Minus, Times, Constant, While, If, Call };
-const char *type_names[] = { "program", "procedure", "stmtlst", "assign", "variable", "plus", "minus", "times", "constant", "while", "if", "call" };
 
 class TNode {
 public:
@@ -13,6 +12,7 @@ public:
 	int value;
 	int statementNumber;
 
+	TNode();
 	//The constructor takes just the NodeType, then we can change the TNode's variables directly
 	TNode(NodeType t);
 };
