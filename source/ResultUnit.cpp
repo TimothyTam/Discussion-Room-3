ac00b827c4cc514071679ac63290d6c07fb3fd08
mrenoon <vunguyenvyvp@gmail.com>
2016-09-13 10:57:33 +0800

    added ResultUnit class

---
+++ source/ResultUnit.cpp
@@ -0,0 +1,10 @@
+#include "ResultUnit.h"
+
+
+
+
+ResultUnit::ResultUnit(int synId, int val)
+{
+	this->synonymId = synId;
+	this->value = val;
+}
---
+++ source/ResultUnit.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "QueryPair.h"
+
+class ResultUnit
+{
+private:
+	
+	
+public:
+	int synonymId;
+	int value;
+	ResultUnit(int synonymId, int value);
+
+	
+};
+
