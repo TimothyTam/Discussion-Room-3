6a0c74841a12b0b9d4b6423f9c20450d20905cc3
shirlenequah <shirlenequah@yahoo.com.sg>
2016-11-05 18:56:21 +0800

    remove prog_line.stmt#

--- source/QueryValidation.cpp
+++ source/QueryValidation.cpp
@@ -272,7 +272,7 @@ bool QueryValidation::checkattrRef(string select) {
 	case stmtNo: //assign, stmt,call,if,while
 		if ((type == QueryUtility::SYNONYM_TYPE_ASSIGN) || (type == QueryUtility::SYNONYM_TYPE_STMT) ||
 			(type == QueryUtility::SYNONYM_TYPE_CALL) || (type == QueryUtility::SYNONYM_TYPE_IF) ||
-			(type == QueryUtility::SYNONYM_TYPE_WHILE) || (type == QueryUtility::SYNONYM_TYPE_PROG_LINE)) {
+			(type == QueryUtility::SYNONYM_TYPE_WHILE) ) {
 			selectList = syn;
 			return true;
 		}
@@ -558,7 +558,7 @@ int QueryValidation::isString(string arg) {
 	case none:
 		return -1;
 	case stmtNo:
-		if ((syn == QueryUtility::SYNONYM_TYPE_STMT) || (syn == QueryUtility::SYNONYM_TYPE_CALL)|| (syn == QueryUtility::SYNONYM_TYPE_PROG_LINE) || (syn == QueryUtility::SYNONYM_TYPE_ASSIGN) || (syn == QueryUtility::SYNONYM_TYPE_WHILE) || (syn == QueryUtility::SYNONYM_TYPE_IF)) {
+		if ((syn == QueryUtility::SYNONYM_TYPE_STMT) || (syn == QueryUtility::SYNONYM_TYPE_CALL) || (syn == QueryUtility::SYNONYM_TYPE_ASSIGN) || (syn == QueryUtility::SYNONYM_TYPE_WHILE) || (syn == QueryUtility::SYNONYM_TYPE_IF)) {
 			return 0;
 		}
 	case value:
